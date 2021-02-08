308c67d696a757e743e2e54936386982b08e78cf
Alexander <abuabara@users.noreply.github.com>
2021-02-07 18:19:38 -0600

    Update plain-clip.rb
    The link to the app needs to be updated, from .dmg to .zip.

--- Casks/plain-clip.rb
+++ Casks/plain-clip.rb
@@ -2,7 +2,7 @@ cask "plain-clip" do
   version :latest
   sha256 :no_check
 
-  url "https://www.bluem.net/files/Plain-Clip.dmg"
+  url "https://www.bluem.net/files/plain-clip.zip"
   name "Plain Clip"
   homepage "https://www.bluem.net/en/mac/plain-clip/"
 
